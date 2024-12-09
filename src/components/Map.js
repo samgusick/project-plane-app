1d2203842ebf7c662d54bef2772a18d57d462557
Sam Gusick <sgusick@uvm.edu>
2024-12-08 20:34:18 -0500

    consolidated Map.js, still working on hover/click icon changes

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,11 +1,9 @@
-import ErrorPopup from "./ErrorPopup.js";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import L from "leaflet";
-import { OPENSKY_CREDENTIALS } from "../config/config";
-import betaLogo from "../images/beta_air_llc_logo.png";
-import betaLogoShadow from "../images/beta_air_llc_logo_shadow.png";
-import betaLogoOrange from "../images/beta_air_llc_logo_shadow_orange.png"
 import "leaflet-rotatedmarker";
+import { OPENSKY_CREDENTIALS } from "../config/config";
+import betaLogoShadow from "../images/beta_air_llc_logo_shadow.png";
+import betaLogoOrange from "../images/beta_air_llc_logo_shadow_orange.png";
 
 const Map = ({
   selectedMarker,
@@ -19,9 +17,10 @@ const Map = ({
   mapRef,
   resetMapView,
 }) => {
-  const markersRef = useRef([]);
+  const markersRef = useRef({});
 
   useEffect(() => {
+    // Initialize map instance if it doesn't already exist
     if (!mapRef.current) {
       const mapInstance = L.map("map", {
         center: [44.0, -72.7],
@@ -43,24 +42,20 @@ const Map = ({
       try {
         const { username, password } = OPENSKY_CREDENTIALS;
         const credentials = btoa(`${username}:${password}`);
-
-        const lamin = 42.7;
-        const lamax = 45.0;
-        const lomin = -73.4377;
-        const lomax = -71.4657;
+        const bounds = {
+          lamin: 42.7,
+          lamax: 45.0,
+          lomin: -73.4377,
+          lomax: -71.4657,
+        };
 
         const response = await fetch(
-          `https://opensky-network.org/api/states/all?lamin=${lamin}&lomin=${lomin}&lamax=${lamax}&lomax=${lomax}`,
-          {
-            headers: {
-              Authorization: `Basic ${credentials}`,
-            },
-          }
+          `https://opensky-network.org/api/states/all?lamin=${bounds.lamin}&lomin=${bounds.lomin}&lamax=${bounds.lamax}&lomax=${bounds.lomax}`,
+          { headers: { Authorization: `Basic ${credentials}` } }
         );
 
         if (!response.ok) {
           if (response.status === 429) {
-            // Suppress 429 Too Many Requests errors
             console.warn("Rate limit hit. Suppressing log.");
             setLoadFlights(false);
           } else {
@@ -69,12 +64,8 @@ const Map = ({
           return;
         }
 
-        const data = await response.json();
-        const { states } = data;
-
-        // Create a Set of current ICAOs from the new data
+        const { states } = await response.json();
         const currentIcaos = new Set();
-        let updatedSelectedMarker = null;
 
         if (states) {
           states.forEach((plane) => {
@@ -99,12 +90,9 @@ const Map = ({
               category,
             ] = plane;
 
-            // Validate coordinates
             if (isFinite(latitude) && isFinite(longitude)) {
               currentIcaos.add(icao);
-
-              // Check if marker exists
-              let marker = markersRef.current[icao];
+              const existingMarker = markersRef.current[icao];
 
               const orangeIcon = L.icon({
                 iconUrl: betaLogoOrange,
@@ -118,68 +106,35 @@ const Map = ({
                 iconAnchor: [42, 32.5],
               });
 
-              if (marker) {
-                // Update marker position and rotation
-                marker.setLatLng([latitude, longitude]);
-                marker.setRotationAngle(true_track || 0);
-              
-                marker.on("click", () => {
-                  // Reset the previously selected marker's icon
-                  if (selectedMarker && markersRef.current[selectedMarker.icao]) {
-                    markersRef.current[selectedMarker.icao].setIcon(greenIcon);
-                  }
-              
-                  // Update selected marker
-                  setSelectedMarker({
-                    icao,
-                    callsign,
-                    origin_country,
-                    time_position,
-                    last_contact,
-                    longitude,
-                    latitude,
-                    baro_altitude,
-                    on_ground,
-                    velocity,
-                    true_track,
-                    vertical_rate,
-                    sensors,
-                    geo_altitude,
-                    squawk,
-                    spi,
-                    position_source,
-                    category,
-                  });
-                  setSelectedCallsign(callsign);
-              
-                  // Set the new marker's icon to orange
-                  marker.setIcon(orangeIcon);
-                });
-              
-                marker.on("mouseover", () => {
-                    marker.setIcon(orangeIcon);
-                });
-              
-                marker.on("mouseout", () => {
-
-                  const latLng = marker.getLatLng();
-                  if (selectedMarker && [selectedMarker.latitude, selectedMarker.longitude] != [latLng.lat, latLng.lng]) {
-                    marker.setIcon(greenIcon);
-                  }
-                });
+              if (existingMarker) {
+                existingMarker.setLatLng([latitude, longitude]);
+                existingMarker.setRotationAngle(true_track || 0);
               } else {
-                // Create a new marker
-                marker = L.marker([latitude, longitude], {
-                  icon: greenIcon,
+                const marker = L.marker([latitude, longitude], {
+                  icon:
+                    selectedMarker &&
+                    selectedMarker.longitude === longitude &&
+                    selectedMarker.latitude === latitude
+                      ? orangeIcon
+                      : greenIcon,
                 }).addTo(mapRef.current);
-              
+
                 marker.on("click", () => {
-                  // Reset the previously selected marker's icon
-                  if (selectedMarker && markersRef.current[selectedMarker.icao]) {
-                    markersRef.current[selectedMarker.icao].setIcon(greenIcon);
-                  }
-              
-                  // Update selected marker
+                  // Log selectedMarker and clicked marker info
+                  console.log(
+                    "Selected Marker Longitude:",
+                    selectedMarker?.longitude,
+                    "Selected Marker Latitude:",
+                    selectedMarker?.latitude
+                  );
+                  console.log(
+                    "Clicked Marker Longitude:",
+                    longitude,
+                    "Clicked Marker Latitude:",
+                    latitude
+                  );
+
+                  // Set the clicked marker as selected
                   setSelectedMarker({
                     icao,
                     callsign,
@@ -201,107 +156,23 @@ const Map = ({
                     category,
                   });
                   setSelectedCallsign(callsign);
-              
-                  // Set the new marker's icon to orange
                   marker.setIcon(orangeIcon);
                 });
-              
-                marker.on("mouseover", () => {
-                  if (!selectedMarker || selectedMarker.icao !== icao) {
-                    marker.setIcon(orangeIcon);
-                  }
-                });
-              
-                marker.on("mouseout", () => {
-                  // Do not reset the icon if this is the selected marker
-                  if (!selectedMarker || selectedMarker.icao !== icao) {
-                    marker.setIcon(greenIcon);
-                  }
-                });
-              
+
                 markersRef.current[icao] = marker;
               }
-              
-              
-              
-
-              // Track previously selected marker
-              if (callsign === selectedCallsign) {
-                updatedSelectedMarker = {
-                  icao,
-                  callsign,
-                  origin_country,
-                  time_position,
-                  last_contact,
-                  longitude,
-                  latitude,
-                  baro_altitude,
-                  on_ground,
-                  velocity,
-                  true_track,
-                  vertical_rate,
-                  sensors,
-                  geo_altitude,
-                  squawk,
-                  spi,
-                  position_source,
-                  category,
-                };
-                // mapRef.current.setView([latitude, longitude], 12);
-              }
-
-              // Update pinned flights
-              setPinnedFlights((prevPinnedFlights) =>
-                prevPinnedFlights.map((flight) =>
-                  flight.callsign === callsign
-                    ? {
-                        ...flight,
-                        icao,
-                        callsign,
-                        origin_country,
-                        time_position,
-                        last_contact,
-                        longitude,
-                        latitude,
-                        baro_altitude,
-                        on_ground,
-                        velocity,
-                        true_track,
-                        vertical_rate,
-                        sensors,
-                        geo_altitude,
-                        squawk,
-                        spi,
-                        position_source,
-                        category,
-                      }
-                    : flight
-                )
-              );
-            } else {
-              console.warn(`Skipping plane with invalid coordinates: ${plane}`);
             }
           });
         }
 
+        // Remove outdated markers
         Object.keys(markersRef.current).forEach((icao) => {
-          const marker = markersRef.current[icao];
-          if (marker) {  // Check if marker exists
-            if (!currentIcaos.has(icao)) {
-              if (icao == selectedMarker?.icao) {  // Optional chaining to avoid issues if selectedMarker is undefined
-                resetMapView();
-              }
-              mapRef.current.removeLayer(marker);
-              delete markersRef.current[icao];
-            }
+          if (!currentIcaos.has(icao)) {
+            const marker = markersRef.current[icao];
+            mapRef.current.removeLayer(marker);
+            delete markersRef.current[icao];
           }
         });
-        
-
-        // Update selected marker
-        if (updatedSelectedMarker) {
-          setSelectedMarker(updatedSelectedMarker);
-        }
       } catch (error) {
         if (error.message.includes("429")) {
           console.warn("Rate limit hit. Suppressing error log.");
@@ -310,21 +181,20 @@ const Map = ({
         }
       }
     };
-    
+
     if (loadFlights) {
       fetchPlaneData();
       const intervalId = setInterval(fetchPlaneData, 30000);
-  
       return () => clearInterval(intervalId);
     }
   }, [
-    selectedCallsign,
+    selectedMarker,
     setPinnedFlights,
     setSelectedCallsign,
     setSelectedMarker,
     mapRef,
     loadFlights,
-    setLoadFlights
+    setLoadFlights,
   ]);
 
   return <div id="map" style={{ height: "100%", width: "100%" }} />;
